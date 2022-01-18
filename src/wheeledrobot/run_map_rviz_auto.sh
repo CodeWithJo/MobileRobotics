31a30160f8658a65f27d5a647dcedd6d96c08b96
CodeWithJo <63004729+CodeWithJo@users.noreply.github.com>
2022-01-18 10:03:07 +0200

    Delete src directory

--- src/wheeledrobot/run_map_rviz_auto.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd FinalProject_ws
-source devel/setup.bash
-roslaunch follow_waypoints follow_waypoints.launch
-roslaunch wheeledrobot map_rviz_amcl.launch
-rostopic pub /start_journey std_msgs/Empty -1
-wait
-
--- src/wheeledrobot/run_teleop.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-roslaunch wheeledrobot teleop.launch
